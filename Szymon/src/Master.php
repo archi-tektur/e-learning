eca94a214492b7b16e16b9be21d2ba139188716e
Oskar 'archi_tektur' Barcz <kontakt@archi-tektur.pl>
2020-01-15 13:30:06 +0100

    Add composer simple config

---
+++ Szymon/src/Master.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+/**
+ * Master
+ */
+class Master
+{
+    /** @return string */
+    public function index(): string
+    {
+        return 'Hello, World!';
+    }
+}
